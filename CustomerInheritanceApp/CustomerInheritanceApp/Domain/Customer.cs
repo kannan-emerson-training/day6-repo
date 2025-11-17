ec47dbeba7ee001cc2b66b5504e0600abf10d2dc
kannan <kannans89@hotmail.com>
2025-11-17 11:36:04 +0530

    inheritance

---
+++ CustomerInheritanceApp/CustomerInheritanceApp/Domain/Customer.cs
@@ -0,0 +1,55 @@
+﻿
+
+using System;
+using System.Data.Common;
+
+namespace CustomerInheritanceApp.Domain
+{
+    class Customer : Object // ISA
+    {
+
+        private string _id;
+        private string _name;
+        private string _location;
+
+
+        public Customer(string id, string name):this(id,name,"Nodia")
+        {
+           
+           
+
+
+        }
+
+        public Customer(string id, string name, string _location)
+        {
+
+            _id = id;
+            _name = name;
+           this. _location = _location;
+        }
+      
+
+        public string Id
+        {
+            get { return _id; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string Location
+        {
+
+            get { return _location; }
+        }
+
+        public override string ToString()
+        {
+            return $"id is {_id} , name is {_name} ,location {_location},parent {base.ToString()}";
+        }
+
+    }
+}
--- CustomerInheritanceApp/CustomerInheritanceApp/Program.cs
+++ CustomerInheritanceApp/CustomerInheritanceApp/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace CustomerInheritanceApp
+﻿using CustomerInheritanceApp.Domain;
+using System;
+
+namespace CustomerInheritanceApp
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Customer c1 = new Customer("C2001", "Hemang");
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c1.GetType());
+           
+
+
+
+        }
+        static void CaseStudy1() {
+
+            PrintInfo(new Customer("C2001", "Hemang"));
+            PrintInfo(new Customer("C3001", "Preeti", "Delhi"));
+        }
+        static void PrintInfo(Customer customer)
+        {
+            Console.WriteLine("id :"+customer.Id);
+            Console.WriteLine("name :"+customer.Name);
+            Console.WriteLine("location :" + customer.Location);
         }
     }
 }
