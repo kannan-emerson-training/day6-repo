cb1dc25f8cad973ccd332c3ee12474673861a781
kannan <kannans89@hotmail.com>
2025-11-17 10:29:20 +0530

    day6 first

---
+++ CustomerInheritanceApp/CustomerInheritanceApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomerInheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
